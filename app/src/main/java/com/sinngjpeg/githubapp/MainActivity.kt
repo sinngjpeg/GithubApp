05db2d23385f58a23eb631497067557fa155bf42
sinng <ingridsilvatec@outlook.com>
2022-06-12 00:33:02 -0300

    adiciona cores do projeto

--- app/src/main/java/com/sinngjpeg/githubapp/MainActivity.kt
+++ app/src/main/java/com/sinngjpeg/githubapp/MainActivity.kt
@@ -5,14 +5,23 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.sinngjpeg.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var analytics: FirebaseAnalytics
 
+    object Constants {
+        const val THREAD_SLEEP_SPLASH_SCREEN = 1000L
+    }
+
+    private lateinit var analytics: FirebaseAnalytics
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        Thread.sleep(Constants.THREAD_SLEEP_SPLASH_SCREEN)
+        setTheme(R.style.Theme_GithubApp)
+        setContentView(binding.root)
         analytics = FirebaseAnalytics.getInstance(this)
     }
 }
